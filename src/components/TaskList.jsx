0f4e258a925a589a146bad0b7dfa159810f40b16
Caleb Parrra <mccalebyt@gmail.com>
2022-07-09 10:37:24 -0500

    delete task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {deleteTask} from '../features/tasks/taskSlice'
 
 function TaskList() {
 
   const tasks = useSelector(state => state.tasks);
-  console.log(tasks)
+  const dispatch = useDispatch()
+
+  const handleDelete = (id) =>{
+    dispatch(deleteTask(id))
+  }
+
   return (
     <div>
       {tasks.map(task => {
@@ -11,6 +17,7 @@ function TaskList() {
           <div key={task.id}>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
+            <button onClick={()=>handleDelete(task.id)}>Delete</button>
           </div>
         )
       })
